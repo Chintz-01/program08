2751f08e295f4766875ed2317b016542786965b2
Chintz-01 <pmskec@gmail.com>
2025-06-03 12:08:42 +0530

    Update App.java

--- program08/src/main/java/Chintz-01/App.java
+++ program08/src/main/java/Chintz-01/App.java
@@ -1,13 +1,17 @@
 package Chintz-01;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        number=[6,10,20,5,30,60,17,35]
+            for (i=0;i<=7;i++)
+            if(i%10==0){
+                System.out.println("Number is disible by 10");
+            }
+        else{
+            System.out.println("Number is not divisible by 10");
+        }
+        print(numbers)
     }
 }
