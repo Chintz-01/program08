120bd933ca93f507064ea10fc862b96800307ac2
Chintz-01 <pmskec@gmail.com>
2025-06-03 12:34:14 +0530

    Update App.java

--- program08/src/main/java/Chintz-01/App.java
+++ program08/src/main/java/Chintz-01/App.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
-public class DivisibilityBy10 {
+public class program {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter an integer: ");
         int number = scanner.nextInt();
 
-        if (isDivisibleBy10(number)) {
+        if (program(number)) {
             System.out.println(number + " is divisible by 10.");
         } else {
             System.out.println(number + " is not divisible by 10.");
@@ -15,7 +15,7 @@ public class DivisibilityBy10 {
 
         scanner.close();
     }
-    public static boolean isDivisibleBy10(int num) {
+    public static boolean program(int num) {
         return num % 10 == 0;
     }
 }
